3e2cb5101e906442a6627e3a24e6d4466bc0a0e0
Peter Alderson <peter.alderson@gmail.com>
2013-09-20 14:40:54 -0400

    initial commit

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,6 @@
 # Be sure to restart your server when you modify this file.
 
-Tutorli::Application.config.session_store :cookie_store, :key => '_swarvy_session'
+Tutorli::Application.config.session_store :cookie_store, :key => '_tutorli_session'
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
